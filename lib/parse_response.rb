f415476ba7b8c0a19c3bff9d40f8d7a9ef9f4ac3
Shelby Kelly <shelby.kelly@colorado.edu>
2014-09-22 10:25:11 -0600

    im a boss

---
+++ lib/parse_response.rb
@@ -0,0 +1,36 @@
+class HTTPResponseParser
+
+  def initialize(file)
+    @file = file
+  end
+
+  def response_code
+    @file.split(" ")[1].to_i
+  end
+
+  def version
+    @file.split(" ")[0]
+  end
+
+  def body
+    @file.split("\n\n")[1].delete("\n")
+  end
+
+  def header
+    header = @file.split("\n")[1..7].map { |line| [line.split(": ")[0] + ":", line.split(": ")[1]] }
+    Hash[header]
+  end
+
+  def content_type
+    header["Content-Type:"]
+  end
+
+  def server
+    header["Server:"]
+  end
+
+  def location
+    header["Location:"]
+  end
+
+end
